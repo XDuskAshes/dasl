ca40f6eee6ab37788948c2e56059a6f59d996858
XDuskAshes <thegaymeradummy@gmail.com>
2023-07-22 17:10:43 -0400

    readme, finish install

---
+++ REAME.md
@@ -0,0 +1,5 @@
+# Dusks Alternative Shell Look
+
+Yes, it's garbage.
+
+Install script in the repo.
--- install.lua
+++ install.lua
@@ -1,3 +1,19 @@
 local l = {
-    [ "/startup.lua" ] = "https://raw.githubusercontent.com/XDuskAshes/dasl/main/startup.lua"
+    [ "/startup.lua" ] = "https://raw.githubusercontent.com/XDuskAshes/dasl/main/startup.lua",
+    [ "/dasl/.config" ] = "https://raw.githubusercontent.com/XDuskAshes/dasl/main/dasl/.config",
+    [ "/dasl/dasl.lua" ] = "https://raw.githubusercontent.com/XDuskAshes/dasl/main/dasl/dasl.lua",
+    [ "/dasl/info.txt" ] = "https://raw.githubusercontent.com/XDuskAshes/dasl/main/dasl/info.txt"
 }
+
+if fs.exists("/dasl/") then
+    for k,v in pairs(l) do
+        shell.run("wget",v,k)
+    end
+else
+    fs.makeDir("/dasl/")
+        for k,v in pairs(l) do
+            shell.run("wget",v,k)
+        end
+end
+
+print("dasl installed.")
