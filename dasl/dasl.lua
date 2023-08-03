10cafcc5ff7dcb3fe1602e90335104946e24087c
XDuskAshes <thegaymeradummy@gmail.com>
2023-08-03 19:02:43 -0400

    Some changes
    1. the "bash" styling now has the cursor as "$" instead of "-$"
    2. path completion!
    3. updated installer

--- dasl/dasl.lua
+++ dasl/dasl.lua
@@ -2,6 +2,8 @@ local function e(s)
     return s == nil or s == ""
 end
 
+local dir
+
 local function cursor()
     local handle = fs.open("/dasl/.config","r")
     local curs = handle.readLine()
@@ -9,7 +11,6 @@ local function cursor()
     handle.close()
 
     if e(style) then
-        local dir
             if shell.dir() == "" then
                 dir = "/"
             else
@@ -29,7 +30,7 @@ local function cursor()
         term.setTextColor(colors.white)
         write(dir)
     elseif style == "bash" then
-        write("-$")
+        write("$")
     elseif style == "cash" then
         write("#")
     end
@@ -45,7 +46,13 @@ end
 
 while true do
     cursor()
-    local i = read()
+    local i = read(nil, nil, function(str)
+        return fs.complete(str, dir, {
+            include_files = true,
+            include_dirs = true,
+            include_hidden = true,
+        })
+    end)
     if i == "cls" then
         term.clear()
         term.setCursorPos(1,1)
@@ -53,6 +60,12 @@ while true do
         pwd()
     elseif i == "cfgedt" then
         shell.run("edit /dasl/.config")
+    elseif e(i) then
+        write("")
+    elseif fs.isDir(i) then
+        shell.run("cd",i)
+    elseif i == ".." then
+        shell.run("cd ..")
     else
         shell.run(i)
     end
--- install.lua
+++ install.lua
@@ -1,20 +1,51 @@
 local l = {
-    [ "/startup.lua" ] = "https://raw.githubusercontent.com/XDuskAshes/dasl/main/startup.lua",
-    [ "/dasl/.config" ] = "https://raw.githubusercontent.com/XDuskAshes/dasl/main/dasl/.config",
-    [ "/dasl/dasl.lua" ] = "https://raw.githubusercontent.com/XDuskAshes/dasl/main/dasl/dasl.lua",
-    [ "/dasl/info.txt" ] = "https://raw.githubusercontent.com/XDuskAshes/dasl/main/dasl/info.txt"
+    "/startup.lua",
+    "/LICENSE",
+    "/dasl/.config",
+    "/dasl/dasl.lua",
+    "/dasl/info.txt",
 }
 
+local spinner = {"|","/","-","\\"}
+local stage = 0
+
+local x,y = term.getCursorPos()
 if fs.exists("/dasl/") then
     for k,v in pairs(l) do
-        shell.run("wget",v,k)
-        sleep(0.1)
+        local handle = assert(http.get("https://raw.githubusercontent.com/XDuskAshes/dasl/"..v))
+            local toWrite = {}
+            repeat
+                local a = handle.readLine()
+                table.insert(toWrite,a)
+            until a == nil
+            handle.close()
+            handle = fs.open(v,"w")
+            for _,v in pairs(toWrite) do
+                handle.writeLine(v)
+            end
+            handle.close()
+        term.setCursorPos(x,y)
+        stage = (stage % 4) + 1
+        write("installing dasl... ".. spinner[stage].." ")
     end
 else
     fs.makeDir("/dasl/")
         for k,v in pairs(l) do
-            shell.run("wget",v,k)
-            sleep(0.1)
+            local handle = assert(http.get("https://raw.githubusercontent.com/XDuskAshes/dasl/"..v))
+            local toWrite = {}
+            repeat
+                local a = handle.readLine()
+                table.insert(toWrite,a)
+            until a == nil
+            handle.close()
+            handle = fs.open(v,"w")
+            for _,v in pairs(toWrite) do
+                handle.writeLine(v)
+            end
+            handle.close()
+        term.setCursorPos(x,y)
+        stage = (stage % 4) + 1
+        write("installing dasl... ".. spinner[stage].." ")
         end
 end
 
